adea182bc03440bf432d88fa3f11179e4c86e3ca
Cameron Bernhardt <cambernhardt@me.com>
2017-09-16 18:05:51 -0400

    Update CORS

--- server.js
+++ server.js
@@ -14,10 +14,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', config.url);
-    res.header('Access-Control-Allow-Methods', 'GET');
-    res.header('Access-Control-Allow-Credentials', 'true')
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 
