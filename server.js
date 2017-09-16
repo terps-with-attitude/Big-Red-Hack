726f175d3689f87ecf047843c6fd8e66e8a99e9e
Cameron Bernhardt <cambernhardt@me.com>
2017-09-16 18:10:35 -0400

    Fix CORS

--- server.js
+++ server.js
@@ -9,15 +9,8 @@ const app = require("express")();
 const http = require("http").Server(app);
 const io = require("socket.io")(http);
 const bodyParser = require("body-parser");
-const cors = require("cors");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(cors());
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
 
 // Current active rooms
 let rooms = {};
