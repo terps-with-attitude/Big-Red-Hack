76d95987397bb87fdbd7680eeb10678729104457
Cameron Bernhardt <cambernhardt@me.com>
2017-09-16 05:33:14 -0400

    Fix endpoints

--- server/server.js
+++ server/server.js
@@ -44,10 +44,6 @@ app.post("/createroom", (req, res) => {
     }
 });
 
-app.get("/", (req, res) => {
-    res.status(200).send("This is Lecture.me's server.");
-})
-
 // Start the app
 app.listen(config.port, () => {
     console.log(`Server started on port ${config.port}`);
